2b2f53ea47fae233675edeef68fc62b3dc6250da
Brett Weisberg <weisberg.brett@gmail.com>
2018-02-27 23:30:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,10 +24,11 @@ end
 
 
 def printer(names)
-   batch_badge_creator(names). each do |badge|
-     puts badge
-   end
+   
   assign_rooms(names).each do |name|
     puts name.chars[-2]
+  end
+  batch_badge_creator(names). each do |badge|
+    puts badge
   end
 end
